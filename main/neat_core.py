f4eb77591e8a93169965f115eadf5207dc87bff5
jawadefaj <jawadefaj006@gmail.com>
2019-02-22 16:19:10 -0800

    restart and trapping problem fix
    trapping problem is fixed but I have not rigorously checked the code. Can sometimes provide weird result.

--- main/neat_core.py
+++ main/neat_core.py
@@ -1,13 +1,20 @@
 import neat
+import os
+import time
+import pyautogui
 from game_running import run_game
+
 
 PROGRAM_NAME = 'Syobon Action (??????????)'
 
 
 def eval_genomes(genomes, config):
 	for genome_id, genome in genomes:
+		print("ID " , genome_id)
 		network = neat.nn.FeedForwardNetwork.create(genome, config)
+		print("network " , network)
 		genome.fitness = run_game(PROGRAM_NAME, network)
+	print("Out of the loop")
 
 
 def run(config_file):
@@ -18,13 +25,24 @@ def run(config_file):
 	population = neat.Population(config)
 
 	# Run for up to 100 generations.
-	winner = population.run(eval_genomes, 100)
+	winner = population.run(eval_genomes, 2)
+
 
 	winner_net = neat.nn.FeedForwardNetwork.create(winner, config)
 
 
+def open_game():
+	cwd = os.getcwd()
+	os.chdir("C:\\Users\\abjaw\\Downloads\\SyobonAction_rc2_win32bin")
+	os.startfile("C:\\Users\\abjaw\\Downloads\\SyobonAction_rc2_win32bin\\OpenSyobonAction.exe")
+	time.sleep(1.0)
+	os.chdir(cwd)
+	pyautogui.keyDown('enter')
+	pyautogui.keyUp('enter')
+
+
 def main():
-        
+	open_game();
 	run('config')
 
 
